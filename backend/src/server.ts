abd505717729bcf40f7a365334563c5f901e474b
Sazzad <netsazzad@gmail.com>
2021-10-21 21:01:06 +0600

    graphql server setup is done

---
+++ backend/src/server.ts
@@ -0,0 +1,42 @@
+import app from "./app";
+import { config } from "dotenv";
+import resolvers from "./gql/resolvers";
+import { ApolloServer, gql } from "apollo-server-express";
+import path from "path";
+import fs from "fs";
+
+config();
+
+(async () => {
+  const typeDefs = gql(
+    fs.readFileSync(path.resolve(__dirname, "gql", "schemas/schema.gql"), {
+      encoding: "utf-8",
+    }),
+  );
+
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req, res }) => ({ req, res }),
+    formatError(err) {
+      return {
+        message: err.message,
+        locations: err.locations,
+        path: err.path,
+      };
+    },
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, path: "/graphql" });
+  const PORT: string = process.env.PORT || "";
+  app
+    .listen(PORT, () => {
+      console.log("##########################################################");
+      console.log("#####               STARTING GRAPHQL SERVER          #####");
+      console.log("##########################################################\n");
+      console.log(`Server running → PORT ${PORT} ${apolloServer.graphqlPath}`);
+    })
+    .on("error", e => {
+      console.error(e);
+    });
+})();
