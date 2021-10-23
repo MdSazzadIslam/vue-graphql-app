aa54ebf01924201a30c75ff7bf23be68198ff618
Sazzad <netsazzad@gmail.com>
2021-10-23 23:18:20 +0600

    apollo client implemented

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import VueApollo from "vue-apollo";
 
-createApp(App).use(router).mount('#app')
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+
+const link = new HttpLink({
+  uri: `http://localhost:5000/graphql`,
+});
+const apolloClient = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+});
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
+createApp(App)
+  .use(router, VueApollo, apolloProvider)
+  .mount("#app");
