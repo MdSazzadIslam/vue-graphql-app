dc8ed1a29eb35dcbab7ace3b068d2325470441c4
Sazzad <netsazzad@gmail.com>
2021-10-24 20:48:57 +0600

    machines query implementation is done

--- frontend/src/components/Machine.vue
+++ frontend/src/components/Machine.vue
@@ -28,39 +28,11 @@
                   >
                     <td>{{ machine.id }}</td>
                     <td>{{ machine.name }}</td>
-                    <td>{{ machine.sensor }}</td>
+                    <td>{{ machine.sensors }}</td>
                     <td>{{ machine.lastKnownPosition }}</td>
                   </router-link>
                 </tbody>
               </table>
-            </div>
-
-            <div class="card-footer py-4">
-              <nav aria-label="...">
-                <ul class="pagination justify-content-end mb-0">
-                  <li class="page-item disabled">
-                    <a class="page-link" href="#" tabindex="-1">
-                      <i class="fas fa-angle-left"></i>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                  </li>
-                  <li class="page-item active">
-                    <a class="page-link" href="#">1</a>
-                  </li>
-                  <li class="page-item">
-                    <a class="page-link" href="#"
-                      >2 <span class="sr-only">(current)</span></a
-                    >
-                  </li>
-                  <li class="page-item"><a class="page-link" href="#">3</a></li>
-                  <li class="page-item">
-                    <a class="page-link" href="#">
-                      <i class="fas fa-angle-right"></i>
-                      <span class="sr-only">Next</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
             </div>
           </div>
         </div>
@@ -81,8 +53,10 @@ import gql from "graphql-tag";
         query: gql`
           query {
             machines {
+              id
               name
-              sensor {
+              sensors {
+                id
                 name
               }
               lastKnownPosition {
@@ -94,7 +68,7 @@ import gql from "graphql-tag";
         `,
         result(result: ApolloResponse) {
           if (!result.loading) {
-            result.data = result.data.machines;
+            const machines: IMachine[] = result.data.machines;
           }
         },
         // pollInterval: 300, // Continuouse call function ms
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -9,25 +9,28 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import VueApollo from "vue-apollo";
+import gql from "graphql-tag";
 
-Vue.prototype.$appName = "Hi-Cord";
+Vue.prototype.$appName = "A simple vue graphql app";
 const link = new HttpLink({
-  uri: `http://localhost:5000/graphql`,
+  uri: "http://localhost:5000/graphql",
 });
 const apolloClient = new ApolloClient({
   link,
   cache: new InMemoryCache(),
   connectToDevTools: true,
 });
+
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
 });
 
-Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
-
+Vue.use(VueApollo, gql);
+Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  apolloProvider,
   render: (h) => h(App),
 }).$mount("#app");
